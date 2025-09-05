d657cb69ff6bc9dde915c5e473a20bb1bdacbdcf
TalalYaz1 <Talalyazbik2@gmail.com>
2025-09-05 18:37:52 +0100

    FINISH LINEEEE

--- Contact/contact.js
+++ Contact/contact.js
@@ -16,19 +16,3 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
-
-const toggle = document.querySelector(".navbar__toggle");
-const menu = document.querySelector(".off-screen");
-const overlay = document.querySelector(".overlay");
-
-toggle.addEventListener("click", () => {
-  toggle.classList.toggle("active");
-  menu.classList.toggle("active");
-  overlay.classList.toggle("active");
-});
-
-overlay.addEventListener("click", () => {
-  toggle.classList.remove("active");
-  menu.classList.remove("active");
-  overlay.classList.remove("active");
-});
